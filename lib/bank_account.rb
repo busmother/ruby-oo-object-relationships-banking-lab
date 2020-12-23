9ec5a2310db1599b52840d9b619af805476b5835
Ian Marshall <imarshall14@gmail.com>
2020-12-23 17:27:59 -0500

    commit tree aa83b8f8aa525ca884111fd9c7dca07c2c18dabd

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
+require "pry"
 class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name
+
+    def initialize(name, balance=1000)
+        @name = name
+        @balance = balance
+        @status = "open"
+    end
+
+    # def balance
+    #     if balance < 0
+    #         balance = 0
+    #     end
+    # end
+
+    def deposit(amount)
+        self.balance = balance + amount
+    end
+
+    def display_balance
+        "Your balance is $#{balance}."
+    end
+
+    def valid?
+        balance > 0 && status == "open" ? true : false
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 
 end
